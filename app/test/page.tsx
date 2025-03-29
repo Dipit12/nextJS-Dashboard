67e3763442588e27304882187f6d3f29c7893a33
Dipit12 <dipitmadan@gmail.com>
2025-03-30 00:10:53 +0530

    feat: completed till chapter-7

---
+++ app/test/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import styles from '@/app/ui/home.module.css'
+
+export default function Test(){
+    return(
+        <div>
+            <button className = {styles.shape}>Click me</button>
+        </div>
+    )
+}
